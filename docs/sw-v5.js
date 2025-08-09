184564c8eaf4da6c4df688326f83883e28bf3a24
ticob24 <colton.faris24@gmail.com>
2025-08-09 18:21:03 +0100

    Delete docs directory

--- docs/sw-v5.js
+++
@@ -1,3 +0,0 @@
-const CACHE='pp-v5';
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html?v=5','./style.css?v=5','./script.js?v=5','./manifest.webmanifest'])))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
