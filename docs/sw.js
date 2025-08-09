b817ed60ffba7d60c0dcb1f756438bfc5aa0361d
ticob24 <colton.faris24@gmail.com>
2025-08-09 15:43:13 +0100

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,2 +1,2 @@
-self.addEventListener('install', e=>{e.waitUntil(caches.open('pp-v1').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'])))});
+self.addEventListener('install', e=>{e.waitUntil(caches.open('pp-v2').then(c=>c.addAll(['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'])))});
 self.addEventListener('fetch', e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
